1d08f284374e736e77852a3da6c9f50427e22387
Jhonatan Nunez <jmoises.n23@gmail.com>
2024-12-29 22:40:06 +0100

    minor changes in environment export and dbName

--- backend/environment/environment.js
+++ backend/environment/environment.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
-const FRONTEND_URL = process.env.FRONTEND_URL;
-const MONGODB_URI = process.env.MONGODB_URI;
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const {
+  PORT,
+  FRONTEND_URL,
+  MONGODB_URI,
+  JWT_SECRET_KEY
+} = process.env;
 
 module.exports = {
   PORT,
--- backend/mongoConnect.js
+++ backend/mongoConnect.js
@@ -7,7 +7,9 @@ async function mongoConnect() {
     if (!MONGODB_URI) {
       throw new Error("MONGODB_URI must be set");
     }
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGODB_URI, {
+      dbName: "Routine-This"
+    })
     console.log("[+] - Connected to MongoDB");
   } catch (e) {
     throw new Error(`There was an error connecting to MongoDB\n ${e}`);
